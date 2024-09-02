6ddc7cd1319ffa4c11dde2529dff187a01f0a7ab
markgamal32 <96923190+markgamal32@users.noreply.github.com>
2024-09-02 04:07:30 +0300

    adding_sampleData_in_ModelBuilderExtensions

--- linkedin_Learning_Dot_Net_8/Models/ShopContext.cs
+++ linkedin_Learning_Dot_Net_8/Models/ShopContext.cs
@@ -5,8 +5,18 @@ namespace linkedin_Learning_Dot_Net_8.Models
 	public class ShopContext : DbContext
 	{
         public ShopContext(DbContextOptions<ShopContext> options) :base(options) { }
-        
-        public DbSet<Product> Products { get; set; } 
+
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			modelBuilder.Entity<Category>()
+			   .HasMany(c => c.Products)
+			   .WithOne(a => a.Category)
+			   .HasForeignKey(a => a.CategoryId);
+
+			modelBuilder.Seed();
+		}
+
+		public DbSet<Product> Products { get; set; } 
         public DbSet<Category> Categories { get; set; }
     }
 }
